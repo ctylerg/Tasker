e29284509c8fa90a1df1992467d98eccba306a52
James Traver <jtraverjr@gmail.com>
2015-05-28 09:42:18 -0500

    views for angular & backbone

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tasker' });
 });
 
+router.get('/backbone', function(req, res, next) {
+  res.render('backbone', { title: 'Tasker', library: 'Backbone' });
+});
+
+router.get('/angular', function(req, res, next) {
+  res.render('angular', { title: 'Tasker', library: 'Angular' });
+});
+
 module.exports = router;
