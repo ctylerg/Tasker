dfa6b50e8e5ea0a45c0ef3cfa6eb8fc1e10b941b
James Traver <jtraverjr@gmail.com>
2015-05-29 00:34:12 -0500

    working angular crud front end

--- public/javascripts/angular-app.js
+++ public/javascripts/angular-app.js
@@ -2,9 +2,31 @@ var taskerApp = angular.module('taskerApp', []);
 
 
 taskerApp.controller('TaskListCtrl', function ($scope, $http) {
-  $http.get('/api/tasks').success(function(data) {
-    console.log('fetched data')
-    console.log(data);
-    $scope.tasks = data;
-  });
+
+  $scope.fetch = function() {
+    $http.get('/api/tasks').success(function(data) {
+      console.log('fetched data')
+      console.log(data);
+      $scope.tasks = data;
+    });
+  };
+
+  $scope.createTask = function(name, description) {
+    $http.post("/api/tasks/", {name : name, description: description}).success(function(data, status) {
+        $scope.fetch();
+    });
+  };
+
+  $scope.completeTask = function(task) {
+    console.log(task);
+    var answer = confirm("Are you sure you want to complete this task?");
+    if (answer == true) {
+      console.log("Deleted Task with _id:" + task["_id"]);
+      $http.delete('/api/tasks/' + task["_id"]);
+      $scope.fetch();
+    }
+
+  };
+
+  $scope.fetch();
 });
